31ee733996cb16f7c4e0244e0e98b1b7a2a567b3
Angel Hernandez <angel.hernandez.12@live.com>
2023-01-07 18:10:28 -0400

    First commit

---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+})
+
+export default router
